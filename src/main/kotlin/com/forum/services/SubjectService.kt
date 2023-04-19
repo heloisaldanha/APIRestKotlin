453be6129db2e950067a2dcef7f9a2807a38ec85
heloisaldanha <heloisa.saldanha@gmail.com>
2023-04-18 21:24:33 -0300

    calling by id on controller and service

--- src/main/kotlin/com/forum/services/SubjectService.kt
+++ src/main/kotlin/com/forum/services/SubjectService.kt
@@ -7,8 +7,9 @@ import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
 @Service
-class SubjectService {
-    fun getListSubject(): List<Subject> {
+class SubjectService(private var subjects: List<Subject>) {
+
+    init {
         val subject = Subject(
             id = 1,
             title = "Desenvolvendo API com Kotlin",
@@ -26,6 +27,51 @@ class SubjectService {
             )
         )
 
-        return listOf(subject, subject, subject, subject)
+        val subject2 = Subject(
+            id = 2,
+            title = "Desenvolvendo API com Kotlin 2",
+            message = "Do básico ao deploy",
+            createdAt = LocalDateTime.now(),
+            course = Course(
+                id = 1,
+                name = "Kotlin",
+                category = "Linguagem de programação"
+            ),
+            author = User(
+                id = 1,
+                name = "Heloísa Saldanha",
+                email = "heloisa@saldanha.com"
+            )
+        )
+
+        val subject3 = Subject(
+            id = 3,
+            title = "Desenvolvendo API com Kotlin 3",
+            message = "Do básico ao deploy",
+            createdAt = LocalDateTime.now(),
+            course = Course(
+                id = 1,
+                name = "Kotlin",
+                category = "Linguagem de programação"
+            ),
+            author = User(
+                id = 1,
+                name = "Heloísa Saldanha",
+                email = "heloisa@saldanha.com"
+            )
+        )
+
+        subjects = listOf(subject, subject2, subject3)
+    }
+    fun getListSubject(): List<Subject> {
+        return subjects
+    }
+
+    fun getById(id: Long): Subject {
+        val subjectById = subjects.stream().filter { t ->
+            t.id == id
+        }.findFirst().get()
+
+        return subjectById
     }
 }
