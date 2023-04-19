273a7037f083fd2418c034e4f20a7af0871e0f2c
heloisaldanha <heloisa.saldanha@gmail.com>
2023-04-19 10:50:58 -0300

    create a new object and 'save'

--- src/main/kotlin/com/forum/services/SubjectService.kt
+++ src/main/kotlin/com/forum/services/SubjectService.kt
@@ -1,68 +1,11 @@
 package com.forum.services
 
-import com.forum.models.Course
 import com.forum.models.Subject
-import com.forum.models.User
 import org.springframework.stereotype.Service
-import java.time.LocalDateTime
 
 @Service
-class SubjectService(private var subjects: List<Subject>) {
+class SubjectService(private var subjects: List<Subject> = mutableListOf()) {
 
-    init {
-        val subject = Subject(
-            id = 1,
-            title = "Desenvolvendo API com Kotlin",
-            message = "Do básico ao deploy",
-            createdAt = LocalDateTime.now(),
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "Linguagem de programação"
-            ),
-            author = User(
-                id = 1,
-                name = "Heloísa Saldanha",
-                email = "heloisa@saldanha.com"
-            )
-        )
-
-        val subject2 = Subject(
-            id = 2,
-            title = "Desenvolvendo API com Kotlin 2",
-            message = "Do básico ao deploy",
-            createdAt = LocalDateTime.now(),
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "Linguagem de programação"
-            ),
-            author = User(
-                id = 1,
-                name = "Heloísa Saldanha",
-                email = "heloisa@saldanha.com"
-            )
-        )
-
-        val subject3 = Subject(
-            id = 3,
-            title = "Desenvolvendo API com Kotlin 3",
-            message = "Do básico ao deploy",
-            createdAt = LocalDateTime.now(),
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "Linguagem de programação"
-            ),
-            author = User(
-                id = 1,
-                name = "Heloísa Saldanha",
-                email = "heloisa@saldanha.com"
-            )
-        )
-
-        subjects = listOf(subject, subject2, subject3)
-    }
     fun getListSubject(): List<Subject> {
         return subjects
     }
@@ -73,5 +16,9 @@ class SubjectService(private var subjects: List<Subject>) {
         }.findFirst().get()
 
         return subjectById
+    }
+
+    fun create(subject: Subject) {
+        subjects.plus(subject)
     }
 }
