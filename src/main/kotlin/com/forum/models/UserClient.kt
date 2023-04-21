c5d0a071d1a463502141c5157d6f971123c82174
heloisaldanha <heloisa.saldanha@gmail.com>
2023-04-21 15:50:38 -0300

    starting the user authentication

--- src/main/kotlin/com/forum/models/UserClient.kt
+++ src/main/kotlin/com/forum/models/UserClient.kt
@@ -10,5 +10,6 @@ data class UserClient(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
     val name: String,
-    val email: String
+    val email: String,
+    val password: String
 )
