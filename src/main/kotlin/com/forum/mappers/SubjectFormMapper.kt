9dfbf3518d939bd06788cb7bf4c29d86388c7f1a
heloisaldanha <heloisa.saldanha@gmail.com>
2023-04-21 16:32:25 -0300

    testing user authentication

--- src/main/kotlin/com/forum/mappers/SubjectFormMapper.kt
+++ src/main/kotlin/com/forum/mappers/SubjectFormMapper.kt
@@ -3,13 +3,13 @@ package com.forum.mappers
 import com.forum.dtos.SubjectFormDTO
 import com.forum.models.Subject
 import com.forum.services.CourseService
-import com.forum.services.UserService
+import com.forum.services.UserClientService
 import org.springframework.stereotype.Component
 
 @Component
 class SubjectFormMapper(
     private val courseService: CourseService,
-    private val userService: UserService
+    private val userClientService: UserClientService
 ): Mapper<SubjectFormDTO, Subject> {
 
     override fun map(subject: SubjectFormDTO): Subject {
@@ -17,7 +17,7 @@ class SubjectFormMapper(
             title = subject.title,
             message = subject.message,
             course = courseService.getById(subject.idCourse),
-            user = userService.getById(subject.idUser)
+            user = userClientService.getById(subject.idUser)
         )
     }
 }
